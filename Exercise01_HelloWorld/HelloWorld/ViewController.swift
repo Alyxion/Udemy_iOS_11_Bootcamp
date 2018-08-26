1c3146bfa208fad7b6182584f9d2d8ea5c1e32b0
Michael Ikemann <ikemann@logiball.de>
2018-08-26 16:10:31 +0200

    Initial commit

---
+++ Exercise01_HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Michael Ikemann on 26.03.18.
+//  Copyright © 2018 Michael Ikemann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
